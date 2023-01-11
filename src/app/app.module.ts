5f374bc38f3d8fd392c02da06ef80c4a38019632
Timmeahj <broostim@hotmail.be>
2023-01-11 04:54:24 +0100

    init app

---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BannerComponent } from './banner/banner.component';
+import { MainComponent } from './main/main.component';
+import { AboutComponent } from './about/about.component';
+import { HobbiesComponent } from './hobbies/hobbies.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ResumeComponent } from './resume/resume.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { ContactComponent } from './contact/contact.component';
+import { InterestsComponent } from './interests/interests.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    BannerComponent,
+    MainComponent,
+    AboutComponent,
+    HobbiesComponent,
+    SkillsComponent,
+    ResumeComponent,
+    PortfolioComponent,
+    TestimonialsComponent,
+    ContactComponent,
+    InterestsComponent,
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
