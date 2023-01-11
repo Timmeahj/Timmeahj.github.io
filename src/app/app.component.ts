36866d155657f20957b6e23b262aa55398e9448f
Timmeahj <broostim@hotmail.be>
2023-01-12 00:24:52 +0100

    add testimonial img, profile img, banner img. Fix bugged navbar on resize event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   status: boolean = true;
+  constructor() {
+    if(window.innerWidth < 1200){
+      this.status = false;
+    }
+  }
   clickEvent(){
     this.status = !this.status;
+  }
+
+  onResize($event: UIEvent) {
+    this.status = window.innerWidth >= 1200;
   }
 }
